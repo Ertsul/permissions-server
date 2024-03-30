7fddd8e157f36434657e8a9d00ad93b4e19cd60b
Ertsul <179445828@qq.com>
2024-03-30 17:35:55 +0800

    feat: 删除测试代码

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DogsModule } from './module/dogs/dogs.module';
 import { LoggerMiddleware } from './middleware/logger/logger.middleware';
 import { ConfigModule } from './module/config/config.module';
+import { PermissionsModule } from './module/permissions/permissions.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ResponseInterceptor } from './shared/interceptors/response.interceptor';
 
 @Module({
-  imports: [ConfigModule, DogsModule],
+  imports: [ConfigModule, PermissionsModule],
   controllers: [AppController],
   providers: [
     { provide: APP_INTERCEPTOR, useClass: ResponseInterceptor },
